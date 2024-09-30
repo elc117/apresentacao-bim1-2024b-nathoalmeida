d08caa6992c4981c2ce9aa8795521eef7f45965f
Nathália Zófoli <nathalia.oalmeida@gmail.com>
2024-09-30 09:58:03 -0300

    seg versao - boundingBoxCheck melhorado

--- funcao8.hs
+++ funcao8.hs
@@ -1,5 +1,5 @@
 boundingBoxCheck :: (Float, Float, Float, Float)  -> Bool
-boundingBoxCheck boundingBox = (\(x1, y1, x2, y2) -> x2) boundingBox >= (\(x1, y1, x2, y2) -> x1) boundingBox && (\(x1, y1, x2, y2) -> y2) boundingBox >= (\(x1, y1, x2, y2) -> y1) boundingBox
+boundingBoxCheck boundingBox = (\(x1, y1, x2, y2) -> x2 >= x1 && y2 >= y1) boundingBox
 
 areBoundingBoxesValid :: [(Float, Float, Float, Float)] -> Bool
 areBoundingBoxesValid boundingBoxes = foldl1 (\x y -> x && y && True) (map boundingBoxCheck boundingBoxes)
